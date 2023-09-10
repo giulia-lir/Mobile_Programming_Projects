a507b800617078c20b615f5eec7ca39760309227
giulia-lir <giulia.libe3@gmail.com>
2023-09-10 22:08:07 +0300

    created project folder for class coding 11.9.2023

---
+++ Class_project2/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Class project ready for lesson 11.9</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

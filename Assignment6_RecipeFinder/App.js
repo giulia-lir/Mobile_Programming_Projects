e4e21576e778ad4b77844b0e41124065d547aa13
giulia-lir <giulia.libe3@gmail.com>
2023-09-11 22:43:14 +0300

    ingredient search app using MealDB

--- Assignment6_RecipeFinder/App.js
+++ Assignment6_RecipeFinder/App.js
@@ -1,11 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { ActivityIndicator, Alert, Button, FlatList, Image, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [recipes, setRecipes] = useState([]);
+  const [ingredient, setIngredient] = useState("");
+  const [isVisible, setIsVisible] = useState(false);
+
+  const fetchRecipes = () => {
+    setIsVisible(true);
+    fetch(process.env.EXPO_PUBLIC_API_URL + "?i=" + ingredient)
+    .then(response => response.json())
+    .then(data => {
+      setRecipes(data.meals)
+      setIsVisible(false);
+    })
+    .catch(err => {
+      setIsVisible(false);
+      Alert.alert('Error', 'Something went wrong')
+      console.log(err)
+    });
+  }
+ 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.textInput}
+        value={ingredient}
+        onChangeText={text => setIngredient(text)}
+        placeholder='Keyword' />
+      <Button title='find' onPress={fetchRecipes} />
+      <ActivityIndicator animating={isVisible} size="large"  />
+      <FlatList 
+        data={recipes}
+        renderItem={({item}) => 
+          <View style={styles.recipeContainer}>
+            <Text style={styles.recipeTitle}>{item.strMeal}</Text>
+            <Image
+              style={styles.thumbnail}
+              source={{ uri: item.strMealThumb }}
+            />
+          </View>
+        }
+      />
     </View>
   );
 }
@@ -13,8 +50,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 50,
+    marginBottom: 50,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: 'grey',
+    padding: 5,
+    marginTop: 25,
+    marginBottom: 10,
+    width: 200
+  },
+  recipeContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'grey',
+    paddingBottom: 5,
+    alignItems: 'center'
+  },
+  recipeTitle: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  thumbnail: {
+    width: 75,
+    height: 75,
   },
 });

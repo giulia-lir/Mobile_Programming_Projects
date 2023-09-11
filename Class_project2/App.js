d17b20f4d971f10cf90e6f2cf099be8804820ce5
giulia-lir <giulia.libe3@gmail.com>
2023-09-11 12:48:56 +0300

    class project api render of repositories

--- Class_project2/App.js
+++ Class_project2/App.js
@@ -1,11 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, StyleSheet, Text, Button, TextInput, View, FlatList, ActivityIndicator } from 'react-native';
 
 export default function App() {
+
+  const [repositories, setRepositories] = useState([]);
+  const [keyword, setKeyword] = useState("");
+  const [isVisible, setIsVisible] = useState(false);
+
+  const fetchRepositories = () => {
+    setIsVisible(true);
+    fetch(process.env.EXPO_PUBLIC_API_URL + "?q=" +keyword)
+    .then(response => response.json())
+    .then(data => {
+      setRepositories(data.items)
+      setIsVisible(false);
+    })
+    .catch(err => {
+      setIsVisible(false);
+      Alert.alert('Error', 'Something went wrong')
+      console.log(err)
+    });
+  }
+ 
   return (
     <View style={styles.container}>
-      <Text>Class project ready for lesson 11.9</Text>
-      <StatusBar style="auto" />
+      <ActivityIndicator animating={isVisible} size="large"  />
+      <FlatList 
+        data={repositories}
+        renderItem={({item}) => 
+          <View>
+            <Text style={{ fontSize: 18, fontWeight: 'bold'}}>{item.full_name}</Text>
+            <Text style={{ fontSize: 16 }}>{item.description}</Text>
+          </View>
+        }
+      />
+      <TextInput
+        value={keyword}
+        onChangeText={text => setKeyword(text)}
+        placeholder='Keyword' />
+      <Button title='fetch' onPress={fetchRepositories} />
     </View>
   );
 }
@@ -13,6 +46,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 50,
+    marginBottom: 50,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',

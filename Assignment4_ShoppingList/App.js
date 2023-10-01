0407d2279e6df0b5768a69751aa80b48d8dfffc4
giulia-lir <giulia.libe3@gmail.com>
2023-10-01 14:09:48 +0300

    added list functionalities with flatlist

--- Assignment4_ShoppingList/App.js
+++ Assignment4_ShoppingList/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
 
@@ -9,6 +9,7 @@ export default function App() {
   const addInput = () => {
     if (input.trim() != "") {
       setShoppingList([...shoppingList, input]);
+      console.log(input)
       setInput("");
     }
   }
@@ -20,11 +21,21 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Shopping List:</Text>
-      <TextInput></TextInput>
+      <TextInput 
+        style={styles.inputField}
+        onChangeText={text => setInput(text)}
+        value={input}
+      />
       <View style={styles.buttonRow}>
         <Button title="Add" onPress={addInput} />
         <Button title="Clear" onPress={clearItems} />
       </View>
+      <Text style={styles.itemsText}>Items:</Text>
+      <FlatList
+        style={styles.listSection}
+        data={shoppingList}
+        renderItem={({ item }) => <Text style={styles.itemsText}>{item}</Text>}
+      />
     </View>
   );
 }
@@ -34,17 +45,32 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   title: {
     fontWeight: 'bold',
     fontSize: 30,
     marginBottom: 20
   },
+  inputField: {
+    borderColor: 'grey',
+    borderWidth: 1,
+    width: 200,
+    padding: 5
+  },
   buttonRow: {
     flexDirection: 'row',
     justifyContent: 'space-evenly',
     width: 200,
     marginTop: 20,
+    marginBottom: 20
   },
+  itemsText: {
+    fontSize: 20
+  },
+  listSection: {
+    maxHeight: 250,
+    width: '80%',
+    marginTop: 15
+  }
 });
